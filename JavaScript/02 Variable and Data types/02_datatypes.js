5e6f6455531ce7865dd08655f5aa85652d139eb7
Prince Kumar <pk0834284@gmail.com>
2026-04-06 10:33:04 +0530

    Create 02_datatypes.js

---
+++ JavaScript/02 Variable and Data types/02_datatypes.js
@@ -0,0 +1,38 @@
+// 1 Numbers : Used for integers or decimals.
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your GPA is ${gpa}`);
+
+// To check data type:
+console.log(typeof age); // "number"
+
+// 2 Strings: A series of characters (text), enclosed in single or double quotes.
+
+let firstName = "Distro";
+let favoriteFood = "Pizza";
+let email = "dstro123@gmail.com";
+
+console.log(`Your name is ${firstName}`);
+console.log(`You like ${favoriteFood}`);
+console.log(`Your email is ${email}`);
+
+console.log(typeof firstName); // "string"
+
+// 3 Booleans: Variables that can only be true or false. Often used as "flags."
+
+let online = true;
+let forSale = false;
+let isStudent = true;
+
+console.log(`Bro is online: ${online}`);
+console.log(`Is this car for sale: ${forSale}`);
+console.log(`Enrolled: ${isStudent}`);
+
+console.log(typeof online); // "boolean"
+
+
