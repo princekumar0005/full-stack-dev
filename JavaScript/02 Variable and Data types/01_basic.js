5895726d776777f005178f22a6ddc58f36c25855
Prince Kumar <pk0834284@gmail.com>
2026-04-06 10:10:18 +0530

    01_basic.js

---
+++ JavaScript/02 Variable and Data types/01_basic.js
@@ -0,0 +1,38 @@
+// Basic Variable Declaration and Assignment
+  
+// Variables are containers that store values. There are two steps: declaration (let) and assignment (=). 
+  
+  
+// Declaration
+let x;
+
+// Assignment
+x = 100;
+
+// Both together
+let y = 123;
+
+console.log(x); // 100
+console.log(y); // 123
+
+// Basic variable declaration
+let fullName = "Tony Stark";
+let age = 24;
+let price = 99.99;
+let x = null;
+let y = undefined;
+let isFollow = true;
+
+// Variable naming rules (Case Sensitive) 
+let full_name = "Shradha Khapra";
+let FullName = "Aman Dhattarwal"; 
+
+// Using let (Updated, but not re-declared) 
+let currentAge = 24;
+currentAge = 25; // Allowed
+
+// Using const (Fixed value) 
+const PI = 3.14;
+// PI = 3.15; // This would cause an error
+
+
